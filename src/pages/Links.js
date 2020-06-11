343c598761a0bb2b713424135f78b2d812365d5c
Charles Dulmage <charles@layer7software.ca>
2020-06-11 17:59:35 -0400

    Harrowdeck Images

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -4,6 +4,8 @@ function Links() {
   return (
     <div>
 		<p>This is my Links page.</p>
+    https://www.d20pfsrd.com/magic-items/artifacts/minor-artifacts/deck-of-many-things-harrow
+    https://aonprd.com/SpellDisplay.aspx?ItemName=Harrowing
     </div>
   );
 }
